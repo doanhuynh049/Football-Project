5473885b1808ead94267d97cd5fb065fd8aa05d9
Doan Huynh Quat <quocthien049@gmail.com>
2024-01-28 22:31:41 +0700

    add infomation about the champion

--- src/Main.cpp
+++ src/Main.cpp
@@ -14,15 +14,30 @@ int main()
     }
     std::sort(teams.begin(), teams.end(), [](const Team &a, const Team &b)
               {
+                  if (a.points == b.points)
+                  {
+                      if (a.goalDifference == b.goalDifference)
+                      {
+                          return a.goalsScored > b.goalsScored;
+                      }
+                      return a.goalDifference > b.goalDifference;
+                  }
                   return a.points > b.points; // Sort in descending order of points
               });
 
     for (const auto &team : teams)
     {
-        std::cout << team.name << " - Points: " << team.points 
-                << " +/-: " << team.goalsScored << "/" << team.goalsConceded << ", Wins: " 
-                << team.wins << ", Losses: " << team.losses << ", Draws: " << team.draws << std::endl;
+        std::cout << team.name << " - Points: " << team.points
+                  << " +/-: " << team.goalsScored << "/" << team.goalsConceded << ", Wins: "
+                  << team.wins << ", Losses: " << team.losses << ", Draws: " << team.draws << std::endl;
     }
-
+    if (teams.size()>3 ){
+        Team &champion = teams[0];
+        Team &runnerUp = teams[1];
+        Team &thirdPlace = teams[2];
+        std::cout << "Champion: " << champion.name << " - Points: " << champion.points << std::endl;
+        std::cout << "Runner-Up: " << runnerUp.name << " - Points: " << runnerUp.points << std::endl;
+        std::cout << "Third Place: " << thirdPlace.name << " - Points: " << thirdPlace.points << std::endl;
+    }
     return 0;
 }
